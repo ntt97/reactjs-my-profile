7166392dd9e6bf1dd283fd10bae40f0a457059df
ntt97 <nthanhtodev97@gmail.com>
2020-10-07 20:47:20 +0700

    Updates

---
+++ precache-manifest.1b053b721a7162eb79f73981c729b059.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016c90a519bb412fb0f7477fcc7e24b2",
+    "url": "/reactjs-my-profile/index.html"
+  },
+  {
+    "revision": "0e3f5ef784d3aab9cdd4",
+    "url": "/reactjs-my-profile/static/css/main.c805a85d.chunk.css"
+  },
+  {
+    "revision": "404fd8867f958cefe9c7",
+    "url": "/reactjs-my-profile/static/js/2.c44ac8da.chunk.js"
+  },
+  {
+    "revision": "0e3f5ef784d3aab9cdd4",
+    "url": "/reactjs-my-profile/static/js/main.00cfbaff.chunk.js"
+  },
+  {
+    "revision": "575dae9eb2fde37972ab",
+    "url": "/reactjs-my-profile/static/js/runtime~main.81122daf.js"
+  },
+  {
+    "revision": "2af81a06d714f98850fce8aed8c76cf0",
+    "url": "/reactjs-my-profile/static/media/avatar.2af81a06.jpg"
+  },
+  {
+    "revision": "d2c74e7b430633770db40a3b975ddecc",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Bold.d2c74e7b.otf"
+  },
+  {
+    "revision": "5cf37d909218a592aa661568c9727270",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-DemiBold.5cf37d90.otf"
+  },
+  {
+    "revision": "6faa576bd8cf054aaa8a7a250c1933ff",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Light.6faa576b.otf"
+  },
+  {
+    "revision": "b15969e2dab8d106cbf185ef081ba9f5",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-LightItalic.b15969e2.otf"
+  },
+  {
+    "revision": "028be64712f87065750bcc96cd6ab1b4",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Medium.028be647.otf"
+  },
+  {
+    "revision": "5ea7b2a9b6b7dd2a4f701cf9220c986b",
+    "url": "/reactjs-my-profile/static/media/iCielVAGRoundedNext-Regular.5ea7b2a9.otf"
+  }
+]);
